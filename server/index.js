7c76505578611890cc744cb261859ff11c178866
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-17 22:14:15 +0100

    Added routes and informing middleware message

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const debug = require("debug")("knowledge:server");
+const morgan = require("morgan");
+const debug = require("debug")("things:server");
+const chalk = require("chalk");
+const { generalError, notFoundError } = require("./middlewares/errors");
+const routerThing = require("./routers/thingsRouter");
 
 const app = express();
 
 const setupServer = (port) =>
   new Promise((resolve, reject) => {
     const server = app.listen(port, () => {
-      debug(`Server is ready at http://localhost:${port}/knowledge`);
+      debug(`Server is ready at http://localhost:${port}`);
       resolve();
     });
 
@@ -14,5 +18,26 @@ const setupServer = (port) =>
       reject(new Error(`Error on server: ${error.message}`));
     });
   });
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  debug(chalk.bgBlack.magenta(`A request has arrived to ${req.url}`));
+  next();
+});
+
+app.use(express.json());
+
+app.use("/", routerThing);
+
+app.use(notFoundError);
+app.use(generalError);
+
+// app.use(express.json());
+
+// app.use("/things", routerThing);
+
+// app.use(notFoundError);
+// app.use(generalError);
 
 module.exports = setupServer;
